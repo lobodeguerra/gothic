412c98baf9d7afc93a0d0e64a3dbe92e76dce420
Lobo de Guerra <lobodeguerramx@gmail.com>
2020-11-12 23:07:18 -0600

    Add base files

---
+++ app/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * The gateway.
+ *
+ * PHP version 7.4
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac L. Felix <isaac@lobodeguerra.com>
+ * @copyright 2020 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://gothic.com
+ */
--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-// Silence is golden.
+/**
+ * Silence is golden.
+ *
+ * PHP version 7.4
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac L. Felix <isaac@lobodeguerra.com>
+ * @copyright 2020 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://gothic.com
+ */
