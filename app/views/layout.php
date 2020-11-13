670f717ff62c7e65242bd6840d66c168ed6c2b6d
Lobo de Guerra <lobodeguerramx@gmail.com>
2020-11-12 23:11:04 -0600

    Add main layout file

---
+++ app/views/layout.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The main layout file.
+ *
+ * PHP version 7.4
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac L. Felix <isaac@lobodeguerra.com>
+ * @copyright 2020 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://gothic.com
+ */
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gothic</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+</body>
+</html>
