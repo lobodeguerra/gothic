67e0b061c2861981af21aad09737c5306ee4c7f6
Lobo de Guerra <lobodeguerramx@gmail.com>
2020-11-16 22:04:27 -0600

    Create base test file

---
+++ app/Test/Test.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The main test file.
+ *
+ * PHP version 7.4
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac L. Felix <isaac@lobodeguerra.com>
+ * @copyright 2020 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://gothic.com
+ */
+
+namespace App\Test;
+
+/**
+ * The main test class.
+ *
+ * PHP version 7.4
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac L. Felix <isaac@lobodeguerra.com>
+ * @copyright 2020 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://gothic.com
+ */
+class Test
+{
+}
