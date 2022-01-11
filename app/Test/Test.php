738b93a91c546a62b4f9cbffd29a815ad0e56f0b
Isaac Félix <isaac.felix@lobodeguerra.com>
2022-01-11 17:34:12 -0600

    feat: add Test base class

---
+++ app/Test/Test.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The main Test file.
+ *
+ * PHP version 8.0
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac Félix <isaac.felix@lobodeguerra.com>
+ * @copyright 2022 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://lobodeguerra.com
+ */
+
+namespace Gothic\Test;
+
+/**
+ * The main Test class.
+ *
+ * PHP version 8.0
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac Félix <isaac.felix@lobodeguerra.com>
+ * @copyright 2022 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://lobodeguerra.com
+ */
+class Test
+{
+}
