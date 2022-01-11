8e4a0aaba1c8fcbe8cef34b949111a18e530d929
Isaac Félix <isaac.felix@lobodeguerra.com>
2022-01-11 14:54:06 -0600

    feat: add app entrypoint

---
+++ app/Gothic.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The main Gothic class file.
+ *
+ * PHP version 8.0
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac Félix <isaac.felix@lobodeguerra.com>
+ * @copyright 2022 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://gothic.com
+ */
+
+namespace Gothic;
+
+/**
+ * The main bootstrap class.
+ *
+ * PHP version 8.0
+ *
+ * @category  MVC_Framework
+ * @package   Gothic
+ * @author    Isaac Félix <isaac.felix@lobodeguerra.com>
+ * @copyright 2022 Gothic PHP Framework
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GPLv3
+ * @link      https://gothic.com
+ */
+class Gothic
+{
+    /**
+     * Construct the app.
+     *
+     * @return void
+     */
+    public static function bootstrap()
+    {
+
+    }
+}
